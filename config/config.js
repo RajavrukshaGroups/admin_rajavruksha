93ba5b67593f0c36d36b62917b1e86139c0ba5e6
technical@rajavrukshagroup.in <monica.b.raju001@gmail.com>
2024-12-10 12:05:01 +0530

    Your commit message

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const dbConnect=()=>{
-    const url = cluster_url = 'mongodb+srv://enquiry:mHpnVFW1fNgdla8h@cluster0.osdmv.mongodb.net/';
+    const cluster_url = 'mongodb+srv://enquiry:mHpnVFW1fNgdla8h@cluster0.osdmv.mongodb.net/';
+    // const cluster_url = 'mongodb://localhost:27017/Rajavruksha';
   mongoose.connect(cluster_url)
   .then(() => console.log("Database connected"))
   .catch((err) => console.log(err));
--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,30 @@
-const login =(req,res)=>{
+const Admin = require('../model/adminModel')
+
+const login =async(req,res)=>{
     try {
-        console.log(req.body,'incoming login');
+        const { email, password } = req.body;
+
+        console.log(req.body, 'Incoming data');
+
+        // Check if admin already exists
+        const AdminEmail = await Admin.findOne({ email });
+        console.log(AdminEmail, 'Admin already exists');
+        
+        if (!AdminEmail) {
+            return res.status(400).json({ success: false, message: "Enter Currect email id" });
+        }else if(password!==AdminEmail.password){
+            return res.status(400).json({ success: false, message: "Incorrect password" });
+        }
+        console.log('login successful');
+        
+        res.status(200).json({success:true,message:"Login Success"})
         
     } catch (error) {
-        console.log(error.message);
-        
+        console.error("Error during login:", error.message);
+        res.status(500).json({ success: false, message: "Server error" });
     }
-}
+};
+
 module.exports={
     login
 }
