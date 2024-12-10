39266f268e0979fbc6eb36b27871b4205b863dc0
RajavrukshaGroups <technical@rajavrukshagroup.in>
2024-12-10 11:15:39 +0530

    remove_nodemodules from git and add other files

--- config/config.js
+++ config/config.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const dbConnect=()=>{
-    const url = cluster_url = 'mongodb+srv://enquiry:mHpnVFW1fNgdla8h@cluster0.osdmv.mongodb.net/';
-  mongoose.connect(cluster_url)
-  .then(() => console.log("Database connected"))
-  .catch((err) => console.log(err));
+const dbConnect = () => {
+  const url = cluster_url = 'mongodb+srv://enquiry:mHpnVFW1fNgdla8h@cluster0.osdmv.mongodb.net/';
+  // const url = "mongodb://localhost:27017/RRPL_Admin";
 
-}
-module.exports={ dbConnect}
+  mongoose
+    .connect(cluster_url)
+    .then(() => console.log("Database connected"))
+    .catch((err) => console.log(err));
+};
+module.exports = { dbConnect };
