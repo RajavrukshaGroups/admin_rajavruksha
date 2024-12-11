c8d51a56b3d2da3ae644f9ead4577ededa2944df
technical@rajavrukshagroup.in <monica.b.raju001@gmail.com>
2024-12-11 22:23:30 +0530

    updated url

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+
 const {dbConnect} = require("./config/config.js");
 const userRoute=require('./routes/routes.js') ;
 const app = express();
 const cors = require('cors');
-// const port = 3000; // You can choose any available port
+const port = 3000; // You can choose any available port
 
 app.use(cors()); // Enable CORS for all routes
 
 dbConnect()
 
-app.use(cors({ origin: 'https://adminpanel.rajavrukshagroup.in' }));
+app.use(cors({ origin: 'https://adminpanel.rajavrukshagroup.in/' }));
 app.use(express.json());
 app.use(express.static('public')); 
 app.use('/',userRoute)
