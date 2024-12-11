b40349b1f4dfa880d6fd10e081f0649e500ea806
technical@rajavrukshagroup.in <monica.b.raju001@gmail.com>
2024-12-11 23:06:11 +0530

    updated index

--- index.js
+++ index.js
@@ -1,61 +1,29 @@
-
+// const app = express();
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-const { dbConnect } = require("./config/config.js");
-const userRoute = require('./routes/routes.js');
-const cors = require('cors');
 
+const {dbConnect} = require("./config/config.js");
+const userRoute=require('./routes/routes.js') ;
 const app = express();
+const cors = require('cors');
 const port = 3000; // You can choose any available port
-
-// Connect to the database
-dbConnect();
-
-// Configure CORS
-app.use(cors({
-    origin: 'https://adminpanel.rajavrukshagroup.in', // Allow specific origin
-    credentials: true // Include cookies or authorization headers if needed
-}));
-
-// Middleware for parsing JSON and serving static files
-app.use(express.json());
-app.use(express.static('public'));
-
-// Routes
-app.use('/', userRoute);
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
-
-
-
-
-// // const app = express();
-// require('dotenv').config();
-// const bodyParser = require('body-parser');
-// const express = require('express');
-// const mongoose = require('mongoose');
-
-// const {dbConnect} = require("./config/config.js");
-// const userRoute=require('./routes/routes.js') ;
-// const app = express();
-// const cors = require('cors');
-// const port = 3000; // You can choose any available port
 
 // app.use(cors()); // Enable CORS for all routes
 
-// dbConnect()
+dbConnect()
 
-// app.use(cors({ origin: 'https://adminpanel.rajavrukshagroup.in' }));
-// app.use(express.json());
-// app.use(express.static('public')); 
-// app.use('/',userRoute)
+app.use(cors({
+  origin: 'https://adminpanel.rajavrukshagroup.in', // Allow specific origin
+  credentials: true // Include cookies or authorization headers if needed
+}));
+
+app.use(express.json());
+app.use(express.static('public')); 
+app.use('/',userRoute)
 
 
-// app.listen(port, () => {
-//     console.log(`Server is running at http://localhost:${port}`);
-//   })  
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  })  
